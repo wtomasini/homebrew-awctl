58b545b5716ce77021e2aa02f488e9aa41652bf9
wtomasini <will@bentomasini.com>
2024-07-23 14:22:06 -0700

    add mac binary

--- Formula/awctl.rb
+++ Formula/awctl.rb
@@ -2,11 +2,12 @@ class Awctl < Formula
   desc "Authwise cli utility to manage objects in your tenant"
   homepage "https://authwise.com"
   url "https://github.com/wtomasini/homebrew-awctl/releases/download/v0.0.1-2/homebrew-awctl-0.0.1.tar.gz"
-  sha256 "c1e2bc9d8e7acfaf428a266ceb0a593903aad696d80eb62207f3cfc1fa80f9f7"
+  # sha256 "c1e2bc9d8e7acfaf428a266ceb0a593903aad696d80eb62207f3cfc1fa80f9f7" for linux
+  sha256 "15b02f8bcfabb76a6966d499b5ab9c160dc5c2c98a14662dd8480478effe0868"
   license "MIT"
 
   def install
-    bin.install "awctl"
+    bin.install "awctl-mac"
   end
 
   test do
