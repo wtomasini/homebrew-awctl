a444593a3683187ce93f0789186255227e648a67
wtomasini <will@bentomasini.com>
2024-07-23 13:05:20 -0700

    add awctl

---
+++ Formula/awctl.rb
@@ -0,0 +1,25 @@
+class Awctl < Formula
+  desc "Authwise cli utility to manage objects in your tenant"
+  homepage "https://authwise.com"
+  url "https://github.com/wtomasini/homebrew-awctl/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "34e137fedcbb005dd386e8a84e7fd17befcb464e6582731a7424fdb06a753a93"
+  license "MIT"
+
+  def install
+    echo "maybe this is what breaks?"
+    bin.install "awctl"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test awctl`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
